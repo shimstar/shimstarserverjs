a1dea744b4492add41cb957eb3d22dbbd1237d86
thyshimrod <shimrod@free.fr>
2016-02-15 21:58:08 +0100

    creation class shimuser // modification pour prendre en compte liste user (ajout/deconnexion)

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ var mySqlClient = mysql.createConnection({
   database : "shimstar"
 });
 
+function shimuser(id, name,socket){
+	this.id=id;
+	this.name=name;
+	this.socket=socket;
+}
+
 // Keep track of the chat clients
 var clients = [];
 
@@ -18,7 +24,7 @@ net.createServer(function (socket) {
   socket.name = socket.remoteAddress + ":" + socket.remotePort 
 
   // Put this new client in the list
-  clients.push(socket);
+  //~ clients.push(socket);
 
   // Send a nice welcome message and announce
   //~ socket.write("Welcome " + socket.name + "\n");
@@ -38,21 +44,38 @@ net.createServer(function (socket) {
   });
 	socket.on('error',function(){
 		console.log('socket reset');
-		clients.splice(clients.indexOf(socket), 1);
+		 var foundUser = null;
+		clients.forEach(function (client) {
+
+      if (client.socket === socket) {
+			foundUser = client;
+		};
+
+    });
+	 if (foundUser){
+		clients.splice(clients.indexOf(foundUser), 1);
+		broadcast(foundUser.name + " left the chat.\n");
+	 }
+		//~ clients.splice(clients.indexOf(socket), 1);
 	});
   // Remove the client from the list when it leaves
   socket.on('end', function () {
-    clients.splice(clients.indexOf(socket), 1);
-    broadcast(socket.name + " left the chat.\n");
+	  var foundUser = null;
+	 console.log(foundUser);
+	 if (foundUser){
+		clients.splice(clients.indexOf(foundUser), 1);
+		broadcast(socket.name + " left the chat.\n");
+	 }
   });
   
   function login(sender,jsonObj){
 	  //~ console.log(jsonObj);
-	  var selectQuery = "SELECT * FROM star001_user where star001_name ='" + jsonObj.login + "' and star001_passwd = '" + jsonObj.password +"'";
+	  var selectQuery = "SELECT star001_id,star001_name FROM star001_user where star001_name ='" + jsonObj.login + "' and star001_passwd = '" + jsonObj.password +"'";
 	   var status=0;
+	   var tempUser ;
 		var sqlQuery = mySqlClient.query(selectQuery);
 		sqlQuery.on("result", function(row) {
-
+			tempUser=new shimuser(row.star001_id,row.star001_name,sender);
 		  status=1;
 		});
 		 
@@ -60,7 +83,13 @@ net.createServer(function (socket) {
 			//~ if (status){
 		  //~ mySqlClient.end();
 			//~ }
-		  sender.write('{"code":"1","status":"' + status + '"}');
+			if(status){
+				sender.write('{"code":"1","status":"' + status + '","name":"' + tempUser.name + '","id":"' + tempUser.id + '" }');
+				clients.push(tempUser);
+			}else{
+				sender.write('{"code":"1","status":"' + status + '"}');
+			}
+		  
 		});
 		 
 		sqlQuery.on("error", function(error) {
@@ -74,8 +103,8 @@ net.createServer(function (socket) {
   function broadcast(message, sender) {
     clients.forEach(function (client) {
       // Don't want to send it to sender
-      if (client === sender) return;
-      client.write(message);
+      if (client.socket === sender) return;
+      client.socket.write(message);
     });
     // Log it to the server output too
     process.stdout.write(message)
